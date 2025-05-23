3b40076438c5a524abbbfacbe25bd8d77441a70f
Sergio Misas <misas_virseda@hotmail.es>
2025-05-24 01:55:50 +0200

    add: added change rules command

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,24 @@
 import logging
 import os
-from utils.utils import read_textfile
+from utils.utils import *
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 load_dotenv()
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+
+WAITING_FOR_RULES = 1
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -24,6 +33,31 @@ async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def change_rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Manda un mensaje con las nuevas reglas, o pon /cancel para cancelar"
+    )
+
+    return WAITING_FOR_RULES
+
+
+async def receive_rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    success_write: bool = write_textfile("rules.txt", update.message.text)
+
+    if success_write:
+        await update.message.reply_text("Reglas guardadas correctamente")
+    else:
+        await update.message.reply_text("Ha ocurrido un error guardando las reglas")
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Operación cancelada")
+
+    return ConversationHandler.END
+
+
 if __name__ == "__main__":
     token = os.getenv("TOKEN")
     application = ApplicationBuilder().token(token).build()
@@ -31,7 +65,19 @@ if __name__ == "__main__":
     start_handler = CommandHandler("start", start)
     rules_handler = CommandHandler("rules", rules)
 
+    change_rules_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("change_rules", change_rules)],
+        states={
+            WAITING_FOR_RULES: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, receive_rules)
+            ]
+        },
+        fallbacks={CommandHandler("cancel", cancel)},
+        per_user=True,
+    )
+
     application.add_handler(start_handler)
     application.add_handler(rules_handler)
+    application.add_handler(change_rules_conv_handler)
 
     application.run_polling()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,25 @@
 import os
 
-
-def read_textfile(relative_path: str) -> str:
+def get_path(relative_path:str) -> str:
     current_dir: str = os.path.dirname(os.path.abspath(__file__))
     project_root: str = os.path.join(current_dir, "..")
-    path: str = os.path.join(project_root, relative_path)
+    return os.path.join(project_root, relative_path)
+
+def read_textfile(relative_path: str) -> str:
+    path: str = get_path(relative_path)
     
     with open(path, 'r', encoding="utf-8") as file:
         try:
             return file.read()
         except (FileNotFoundError, IOError):
             return ""
+
+def write_textfile(relative_path: str, new_text: str) -> bool:
+    path: str = get_path(relative_path)
+    
+    with open(path, 'w', encoding="utf-8") as file:
+        try:
+            file.write(new_text)
+            return True
+        except (FileNotFoundError, IOError):
+            return False
